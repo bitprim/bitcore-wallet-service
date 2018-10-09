b4ede74777fc0ce5e99cd67fad6c86505b934a7b
garceri <10517648+garceri@users.noreply.github.com>
2018-10-09 15:13:12 -0300

    Updated pids and logs dir

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p logs
-mkdir -p pids
+mkdir -p /bitprim/logs
+mkdir -p /bitprim/pids
 
 # run_program (nodefile, pidfile, logfile)
 run_program ()
@@ -29,11 +29,11 @@ run_program ()
   fi
 }
 
-run_program locker/locker.js pids/locker.pid logs/locker.log
-run_program messagebroker/messagebroker.js pids/messagebroker.pid logs/messagebroker.log
-run_program bcmonitor/bcmonitor.js pids/bcmonitor.pid logs/bcmonitor.log
-run_program emailservice/emailservice.js pids/emailservice.pid logs/emailservice.log
-run_program pushnotificationsservice/pushnotificationsservice.js pids/pushnotificationsservice.pid logs/pushnotificationsservice.log
-run_program fiatrateservice/fiatrateservice.js pids/fiatrateservice.pid logs/fiatrateservice.log
-run_program bws.js pids/bws.pid logs/bws.log
+run_program locker/locker.js /bitprim/pids/locker.pid /bitprim/logs/locker.log
+run_program messagebroker/messagebroker.js /bitprim/pids/messagebroker.pid /bitprim/logs/messagebroker.log
+run_program bcmonitor/bcmonitor.js /bitprim/pids/bcmonitor.pid /bitprim/logs/bcmonitor.log
+run_program emailservice/emailservice.js /bitprim/pids/emailservice.pid /bitprim/logs/emailservice.log
+run_program pushnotificationsservice/pushnotificationsservice.js /bitprim/pids/pushnotificationsservice.pid /bitprim/logs/pushnotificationsservice.log
+run_program fiatrateservice/fiatrateservice.js /bitprim/pids/fiatrateservice.pid /bitprim/logs/fiatrateservice.log
+run_program bws.js /bitprim/pids/bws.pid /bitprim/logs/bws.log
 
