ea2dc27c4b733cb8e6a34ac82be2c865f80d0a5c
Dario Ramos <dario.fenix@gmail.com>
2018-11-20 17:13:10 -0300

    [KEOK-120] Email notifications

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+var fileConfig = require('config');
+
 var config = {
   basePath: '/bws/api',
   disableLogs: false,
@@ -21,7 +23,7 @@ var config = {
 
   storageOpts: {
       mongoDb: {
-          uri: 'mongodb://mongodb:27017/bws',
+          uri: fileConfig.get('mongodbUrl'),
       },
   },
   lockOpts: {
@@ -40,27 +42,28 @@ var config = {
   blockchainExplorerOpts: {
       btc: {
           livenet: {
-              provider: 'insight',
-              url: 'https://insight.bitpay.com:443',
+              provider: fileConfig.get('explorer.btc.livenet.provider'),
+              url: fileConfig.get('explorer.btc.livenet.url'),
+              webSocketsUrl: fileConfig.get('explorer.btc.livenet.webSocketsUrl')
           },
           testnet: {
-              provider: 'insight',
-              url: 'https://test-insight.bitpay.com:443',
+              provider: fileConfig.get('explorer.btc.testnet.provider'),
+              url: fileConfig.get('explorer.btc.testnet.url'),
               // Multiple servers (in priority order)
               // url: ['http://a.b.c', 'https://test-insight.bitpay.com:443'],
+              webSocketsUrl: fileConfig.get('explorer.btc.testnet.webSocketsUrl')
           },
       },
       bch: {
           livenet: {
-              provider: 'insight',
-              //url: 'https://cashexplorer.bitcoin.com',
-              url: 'https://bch-insight.bitpay.com:443',
-              addressFormat: 'cashaddr', // copay, cashaddr, or legacy
+              provider: fileConfig.get('explorer.bch.livenet.provider'),
+              url: fileConfig.get('explorer.bch.livenet.url'),
+              addressFormat: fileConfig.get('explorer.bch.livenet.addressFormat'),  // copay, cashaddr, or legacy
+              webSocketsUrl: fileConfig.get('explorer.bch.livenet.webSocketsUrl')
           },
           testnet: {
               provider: 'insight',
               url: 'https://tcust05.blockdozer.com:443',
-              //url: 'http://hanchon.io:3001',
               apiPrefix: '/insight-api',
               addressFormat: 'legacy', // copay, cashaddr, or legacy
           },
@@ -78,36 +81,36 @@ var config = {
   pushNotificationsOpts: {
     templatePath: './lib/templates',
     defaultLanguage: 'en',
-    defaultUnit: 'btc',
+    defaultUnit: 'bch',
     subjectPrefix: '',
     pushServerUrl: 'https://fcm.googleapis.com/fcm',
-    authorizationKey: 'AAAAAAAAAAAAgooglekey',
+    authorizationKey: fileConfig.get('pushNotificationsServerKey'),
   },
   fiatRateServiceOpts: {
       defaultProvider: 'BitPay',
       fetchInterval: 60, // in minutes
   },
-  // To use email notifications uncomment this:
-  // emailOpts: {
-  //  host: 'localhost',
-  //  port: 25,
-  //  ignoreTLS: true,
-  //  subjectPrefix: '[Wallet Service]',
-  //  from: 'wallet-service@bitcore.io',
-  //  templatePath: './lib/templates',
-  //  defaultLanguage: 'en',
-  //  defaultUnit: 'btc',
-  //  publicTxUrlTemplate: {
-  //    btc: {
-  //      livenet: 'https://insight.bitpay.com/tx/{{txid}}',
-  //      testnet: 'https://test-insight.bitpay.com/tx/{{txid}}',
-  //    },
-  //    bch: {
-  //      livenet: 'https://bch-insight.bitpay.com/tx/{{txid}}',
-  //      testnet: 'https://test-bch-insight.bitpay.com/tx/{{txid}}',
-  //    }
-  //  },
-  // },
+  emailOpts: {
+   enableEmailNotifications: fileConfig.get('email.enableEmailNotifications'),
+   host: fileConfig.get('email.mailerUrl'),
+   port: fileConfig.get('email.mailerPort'),
+   ignoreTLS: fileConfig.get('email.ignoreTLS'),
+   subjectPrefix: '[Wallet Service]',
+   from: 'info@bitprim.org',
+   templatePath: './lib/templates',
+   defaultLanguage: 'en',
+   defaultUnit: 'bch',
+   publicTxUrlTemplate: {
+     btc: {
+       livenet: 'https://btc.blockdozer.com/tx/{{txid}}',
+       testnet: 'https://tbtc.blockdozer.com/tx/{{txid}}',
+     },
+     bch: {
+       livenet: 'https://blockdozer.com/tx/{{txid}}',
+       testnet: 'https://tbch.blockdozer.com/tx/{{txid}}',
+     }
+   },
+  },
   // To use sendgrid:
   // var sgTransport = require('nodemail-sendgrid-transport');
   // mailer:sgTransport({
